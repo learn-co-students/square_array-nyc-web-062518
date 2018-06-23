4f2d00795ea08ef35fdb8e6ea4179b10352ec932
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-23 20:54:03 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
-def square_array(array)
-  # your code here
+
+def square_array(input_array)
+  a = Array.new
+  index=0
+  input_array.each do |element|
+    a[index] = element*element
+    index+=1
+  end
+  return a
 end
+  
+
